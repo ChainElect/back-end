9828ff65b938c6154a183f846039976e5b8278b3
BorislavChernev <borocool359@gmail.com>
2025-02-11 18:38:04 +0200

    improved initializeModels.js code quality v2

--- utilities/ir/models/initializeModels.js
+++ utilities/ir/models/initializeModels.js
@@ -1,25 +1,25 @@
+/**
+ * @module FaceModelInitializer
+ * @description Loads and initializes face recognition models from disk.
+ */
+
 const faceapi = require("../faceApiSetup");
 const { ERROR_MESSAGES } = require("../../messages/errorMessages");
 const MODEL_PATHS = require("../models/modelPaths");
 
 /**
- * Initializes face recognition models by loading them from disk.
- *
- * @throws {Error} - Throws an error if any model fails to load.
+ * @summary Initializes face recognition models by loading them from disk.
+ * @throws {Error} Throws an error if any model fails to load.
  */
 const initializeModels = async () => {
   try {
-    // Load models required for face recognition and detection
-    await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_PATHS.ssdMobilenetv1); // For selfies
-    await faceapi.nets.tinyFaceDetector.loadFromDisk(
-      MODEL_PATHS.tinyFaceDetector
-    ); // For ID cards
-    await faceapi.nets.faceLandmark68Net.loadFromDisk(
-      MODEL_PATHS.faceLandmark68
-    );
-    await faceapi.nets.faceRecognitionNet.loadFromDisk(
-      MODEL_PATHS.faceRecognition
-    );
+    // Load models required for face recognition and detection in parallel.
+    await Promise.all([
+      faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_PATHS.ssdMobilenetv1), // For selfies
+      faceapi.nets.tinyFaceDetector.loadFromDisk(MODEL_PATHS.tinyFaceDetector), // For ID cards
+      faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_PATHS.faceLandmark68),
+      faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_PATHS.faceRecognition),
+    ]);
 
     console.log("All models loaded successfully.");
   } catch (error) {
