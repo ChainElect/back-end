19839a106ab598019168c7220619fd7d8bccf05d
BorislavChernev <borocool359@gmail.com>
2025-02-11 18:45:03 +0200

    improved faceApiSetup.js code quality

--- utilities/ir/faceApiSetup.js
+++ utilities/ir/faceApiSetup.js
@@ -1,4 +1,8 @@
-// utilities/ir/faceApiSetup.js
+/**
+ * @module faceApiSetup
+ * @description Configures face-api.js to use the 'canvas' package for image manipulation in a Node.js environment.
+ */
+
 const { Canvas, Image, ImageData } = require("canvas");
 const faceapi = require("face-api.js");
 
